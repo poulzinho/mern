03e3c3317cee292689cecd56e52afa0ccd0ac3e5
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2020-03-22 17:56:04 +0100

    chore(webpack): add client and server

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -1,31 +1,31 @@
-const path = require("path");
-const webpack = require("webpack");
-const CURRENT_WORKING_DIR = process.cwd();
+const path = require('path')
+const webpack = require('webpack')
+const CURRENT_WORKING_DIR = process.cwd()
 
 const config = {
-    name: "browser",
-    mode: "development",
-    devtool: "eval-source-map",
+    name: 'browser',
+    mode: 'development',
+    devtool: 'eval-source-map',
     entry: [
-        "react-hot-loader/patch",
-        "webpack-hot-middleware/client?reload=true",
-        path.join(CURRENT_WORKING_DIR, "client/main.js"),
+        'react-hot-loader/patch',
+        'webpack-hot-middleware/client?reload=true',
+        path.join(CURRENT_WORKING_DIR, 'client/main.js'),
     ],
     output: {
-        path: path.join(CURRENT_WORKING_DIR, "/dist"),
-        filename: "bundle.js",
-        publicPath: "/dist/",
+        path: path.join(CURRENT_WORKING_DIR, '/dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/',
     },
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                use: ["babel-loader"],
+                use: ['babel-loader'],
             },
         ],
     },
     plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.NoEmitOnErrorsPlugin()],
-};
+}
 
-module.exports = config;
+module.exports = config
